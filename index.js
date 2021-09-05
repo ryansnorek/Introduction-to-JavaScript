62e9d6f926e23a0b26cb8e3e494a1aa83875b84c
Ryan <snorekr@gmail.com>
2021-09-05 16:50:01 -0400

    Completed Task 2

--- index.js
+++ index.js
@@ -88,11 +88,11 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(age){
+  return age / 7;
 }
 
-
+dogYears(35)
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
